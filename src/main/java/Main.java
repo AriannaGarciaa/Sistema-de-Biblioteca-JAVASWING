232d6d209644556d09afea6a9110c207147ef89f
AriannaGarciaa <seuemail@example.com>
2025-01-27 22:43:33 -0400

    atualização das telas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,7 @@ import View.UsuarioView;
 
 public class Main {
     public static void main(String[] args) {
-        UsuarioView view = new UsuarioView();
-        view.exibirMenu();
+        new Usuario()
     }
 
 }
